55d0e6e5b0c94c7b4618aee6cffa3b3a0142d5fd
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-08-20 16:20:03 +0300

    procces do task2 hmw 9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,12 +6,18 @@ const refs = {
   inputDataTime: document.querySelector('#datetime-picker'),
   btnStart: document.querySelector('[data-start]'),
   timer: document.querySelector('.timer'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
+
 refs.btnStart.addEventListener('click', onClickBtnStart);
 
 refs.btnStart.disabled = true;
 
 const options = {
+  isActive: false,
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
@@ -19,26 +25,43 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0] < options.defaultDate) {
       window.alert('Please choose a date in the future');
+      return;
     }
     refs.btnStart.disabled = false;
+    const startTime = options.defaultDate;
+    const endTime = selectedDates[0];
+    const diferenceTime = endTime - startTime;
+    const time = convertMs(diferenceTime);
 
-    console.log(selectedDates[0]);
-
-    const startTime = Date.now();
-    // console.log(diferenceTime);
-    // console.log(startTime);
-    // console.log(endTime);
-    setInterval(() => {
-      const endTime = Date.now(selectedDates[0]);
-      const diferenceTime = endTime - startTime;
-      console.log(convertMs(diferenceTime));
-    }, 1000);
+    updateClockFace(time);
   },
+  // startTimer() {
+  //   if (this.isActive) {
+  //     return;
+  //   }
+
+  //   const startTime = Date.now();
+  //   this.isActive = true;
+  //   const endTime = Date.now(selectedDates[0]);
+  //   const diferenceTime = endTime - startTime;
+
+  //   console.log(this.isActive);
+
+  //   setInterval(() => {
+  //     console.log(convertMs(diferenceTime));
+  //   }, 1000);
+  // },
 };
+
 flatpickr(refs.inputDataTime, options);
 
-function onClickBtnStart(evt) {
-  console.log('click');
+function onClickBtnStart(evt) {}
+console.log(refs.days.textContent);
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
 }
 
 function convertMs(ms) {
